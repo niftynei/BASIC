fc977df3e868d7b8c08b06b67f53d532e407d68f
dni ⚡ <office@dnilabs.com>
2023-12-14 21:51:37 +0100

    feat: add webinstaller and automatic releases

---
+++ build-webinstaller.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+PROJECT_NAME=basic
+REPO=https://github.com/lnbits/basic/releases/download
+INSTALLER_PATH=./hardware-installer/public/firmware
+
+git clone https://github.com/lnbits/hardware-installer
+
+cp INSTALLER.md ./hardware-installer/public/INSTALLER.md
+cp versions.json ./hardware-installer/src/versions.json
+cp installer/config.js ./hardware-installer/src/config.js
+
+sed -i "s/%title%/$PROJECT_NAME/g" ./hardware-installer/index.html
+
+mkdir -p $INSTALLER_PATH
+for device in $(jq -r '.devices[]' ./hardware-installer/src/versions.json); do
+    for version in $(jq -r '.versions[]' ./hardware-installer/src/versions.json); do
+        mkdir -p $INSTALLER_PATH/$device/$version
+        wget $REPO/$version/$PROJECT_NAME.ino.bin
+        wget $REPO/$version/$PROJECT_NAME.ino.partitions.bin
+        wget $REPO/$version/$PROJECT_NAME.ino.bootloader.bin
+        mv $PROJECT_NAME* $INSTALLER_PATH/$device/$version
+    done
+done
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+command -v arduino-cli >/dev/null 2>&1 || { echo >&2 "arduino-cli not found. Aborting."; exit 1; }
+arduino-cli config --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json init
+arduino-cli core update-index
+arduino-cli core install esp32:esp32
+arduino-cli upgrade
+arduino-cli lib install WebSockets ArduinoJson Keypad uBitcoin
+arduino-cli compile \
+    --library ./libraries/TFT_eSPI \
+    --build-path build --fqbn esp32:esp32:ttgo-lora32 basic
