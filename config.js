50c4729c33ae700fefa75fa87f13f48e2aa284ab
dni ⚡ <office@dnilabs.com>
2023-12-14 22:26:56 +0100

    add config for installer

---
+++ config.js
@@ -0,0 +1,35 @@
+export const addressesAndFiles = [
+  {
+    address: "0x1000",
+    fileName: "basic.ino.bootloader.bin",
+  },
+  {
+    address: "0x8000",
+    fileName: "basic.ino.partitions.bin",
+  },
+  {
+    address: "0xE000",
+    fileName: "boot_app0.bin",
+  },
+  {
+    address: "0x10000",
+    fileName: "basic.ino.bin",
+  },
+];
+
+export const configPath = "elements.json";
+
+export const elements = [
+  {
+    name: "ssid",
+    value: "",
+    label: "WiFi SSID",
+    type: "text",
+  },
+  {
+    name: "wifipassword",
+    value: "",
+    label: "WiFi Password",
+    type: "text",
+  },
+];
